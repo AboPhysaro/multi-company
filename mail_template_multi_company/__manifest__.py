3a60ed81d2c5edc571461db258fb5073dd228033
VicGuillaume <vigu@odoo.com>
2024-12-17 15:57:54 +0100

    [UPG][4384731] mail_template_multi_company: Upgrade module

--- mail_template_multi_company/__manifest__.py
+++ mail_template_multi_company/__manifest__.py
@@ -3,13 +3,15 @@
 
 {
     "name": "Mail Template Multi Company",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/multi-company",
+    "website": "https://github.com/AboPhysaro/multi-company/tree/17.0/mail_template_multi_company",
     "depends": ["mail"],
     "post_init_hook": "post_init_hook",
     "data": ["security/mail_template.xml", "views/mail_template.xml"],
     "development_status": "Beta",
     "maintainers": ["Olivier-LAURENT"],
+    "application": True,
+    "summary": "Allow the creation of mail templates that are accessible only within a predefined company",
 }
